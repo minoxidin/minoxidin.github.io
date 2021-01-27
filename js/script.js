eca65893e5b67859f748b38a8c77f27fe87813ca
minoxidin <78072139+minoxidin@users.noreply.github.com>
2021-01-27 09:31:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+	$('.scrollTo').on('click', function(){
+	    var scroll_el = $(this).attr('href');
+	    if ($(scroll_el).length != 0) {
+	        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	    }
+	    return false;
+	});
+
+	/* timer */
+
+	function update() {
+		var Now = new Date(), Finish = new Date();
+		Finish.setHours( 23);
+		Finish.setMinutes( 59);
+		Finish.setSeconds( 59);
+		if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
+			Finish.setDate( Finish.getDate() + 1);
+		}
+		var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
+		var hrs = Math.floor( sec / 3600);
+		sec -= hrs * 3600;
+		var min = Math.floor( sec / 60);
+		sec -= min * 60;
+		$(".timer .hours").html( pad(hrs));
+		$(".timer .minutes").html( pad(min));
+		$(".timer .seconds").html( pad(sec));
+		setTimeout( update, 200);
+	}
+	function pad(s) {
+		s = ("00"+s).substr(-2);
+		return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
+	}
+	update();
+
+
+});
+
+$(window).on("load", function(){
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		autoHeight: true,
+		smartSpeed: 300,
+		mouseDrag: false,
+		pullDrag: false,
+		nav: true,
+		navText: ""
+	});
+	
+});
